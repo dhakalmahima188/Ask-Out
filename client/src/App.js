08d7f0a8f7d35e3fceb9a2077dd65511c35868dd
smrkarkii <076bct080.smriti@pcampus.edu.np>
2021-10-31 15:38:59 +0545

    register pages with error

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 import Login from "./Components/auth/Login/login";
+import Workspace from "./Components/auth/Workspace/workspace";
+import Register from "./Components/auth/Register/register";
 
 import Main from './Components/main.component.js'
 
@@ -8,8 +10,10 @@ function App() {
   return (
     <>
       <div className="App">
+        <Register/>
+      {/* <Workspace/> */}
       {/* <Login/> */}
-     <Main/>
+     {/* <Main/> */}
       </div>
     </>
   );
