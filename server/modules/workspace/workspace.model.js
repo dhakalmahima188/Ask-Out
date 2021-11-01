4ecfc34e2c6a4197d2c3ecec49ec0a401008472f
Tanisha Chaudhary <tanishach203@gmail.com>
2021-11-02 01:06:13 +0545

    timestamp issue resolved

--- server/modules/workspace/workspace.model.js
+++ server/modules/workspace/workspace.model.js
@@ -1,19 +1,17 @@
 const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 const WorkspaceSchema = new schema({
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    }
-   },{
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  }
+}, {
   writeConcern: {
     w: 'majority',
     j: true,
     wtimeout: 1000
-  }}, 
-  {
-    timestamps: true
+  }
 });
-
+WorkspaceSchema.set('timestamps', true);
 module.exports = mongoose.model("Workspace", WorkspaceSchema)
