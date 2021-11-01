4de22029914af9c200ea13b5310e0eb19ec1bf1d
mahima <076bct033.mahima@pcampus.edu.np>
2021-11-01 16:52:09 +0545

    Employee part Done

--- server/modules/employee/users.controller.js
+++ server/modules/employee/users.controller.js
@@ -1,1 +1,61 @@
 //get post put
+const usersmodel = require('./users.model')
+const mappedData = require('../../helpers/mapEmployee')
+async function getAll(req, res, next) {
+    await usersmodel.find({}).sort({
+            _id: -1,
+        })
+        .exec(function (err, data) {
+            if (err) {
+                return next(err);
+            }
+            res.json(data)
+        })
+}
+
+async function getById(req, res, next) {
+    await usersmodel.findOne({
+            _id: req.params.id
+        })
+        .exec(function (err, data) {
+            if (err) {
+                return next(err);
+            }
+            if (!data) {
+                return next({
+                    msg: "Not Found",
+                    status: 404
+                })
+            }
+            res.json(data)
+        })
+}
+
+// async function getByTag(req, res, next) {
+//   await usersmodel.findOne({
+//           _id: req.params.tag
+//       })
+//       .exec(function (err, data) {
+//           if (err) {
+//               return next(err);
+//           }
+//           if (!data) {
+//               return next({
+//                   msg: "Not Found",
+//                   status: 404
+//               })
+//           }
+//           res.json(data)
+//       })
+// }
+
+
+
+
+
+  
+module.exports = {
+    getAll,
+    getById,
+    //getByTag
+}
--- server/modules/employee/users.model.js
+++ server/modules/employee/users.model.js
@@ -1,1 +1,49 @@
-//schema
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+const EmployeeSchema  = new schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        //unique: true,
+      },
+      username: {
+        type: String,
+        //required: true,
+        unique: true,
+      },
+      workspace_id: {   //post create walA HERA
+        type: String,
+        //required: true,
+      
+      },
+      tag: {   //post create walA HERA
+        type: String,
+        enum:[
+         'IT','FINANCE','HR'
+        ]
+        //required: true,
+      
+      },
+      
+      
+      
+  },
+  {
+    writeConcern: {
+      w: "majority",
+      j: true,
+      wtimeout: 1000,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Employee", EmployeeSchema);
